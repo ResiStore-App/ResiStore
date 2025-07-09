879ac0da95e3d946153a5316e7b1c549d5c95023
iss4gerry <gerrydesrian488@gmail.com>
2025-07-09 12:26:38 +0700

    feat: dashboard page

--- app/Filament/Resources/BuyTransactionResource/Pages/EditBuyTransaction.php
+++ app/Filament/Resources/BuyTransactionResource/Pages/EditBuyTransaction.php
@@ -9,7 +9,22 @@ use Filament\Resources\Pages\EditRecord;
 class EditBuyTransaction extends EditRecord
 {
     protected static string $resource = BuyTransactionResource::class;
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
 
+        $details = $this->record->details()->get()->map(function ($item) {
+            return [
+                'id_barang' => $item->id_barang,
+                'kuantitas' => $item->kuantitas,
+                'harga_satuan' => $item->harga_satuan,
+                'is_new' => false,
+            ];
+        })->toArray();
+
+        $data['details'] = $details;
+
+        return $data;
+    }
     protected function getHeaderActions(): array
     {
         return [
